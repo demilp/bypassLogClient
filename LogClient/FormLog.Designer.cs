819a1d858ba47cfd5abed8e3ef5f5d7c1692ac7f
demilp <demilp@gmail.com>
2017-01-11 14:40:25 -0300

    First commit

---
+++ LogClient/FormLog.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace LogClient
+{
+    partial class FormLog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLog));
+            this.messages = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.clients = new System.Windows.Forms.ListBox();
+            this.btnPlayPause = new System.Windows.Forms.Button();
+            this.textBoxDataPreview = new System.Windows.Forms.TextBox();
+            this.messages.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // messages
+            // 
+            this.messages.Controls.Add(this.dataGridView1);
+            this.messages.Location = new System.Drawing.Point(226, 5);
+            this.messages.Name = "messages";
+            this.messages.Size = new System.Drawing.Size(732, 495);
+            this.messages.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(732, 495);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.clients);
+            this.panel1.Location = new System.Drawing.Point(12, 61);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 622);
+            this.panel1.TabIndex = 1;
+            // 
+            // clients
+            // 
+            this.clients.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.clients.FormattingEnabled = true;
+            this.clients.Location = new System.Drawing.Point(0, 0);
+            this.clients.MultiColumn = true;
+            this.clients.Name = "clients";
+            this.clients.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.clients.Size = new System.Drawing.Size(200, 622);
+            this.clients.TabIndex = 0;
+            this.clients.SelectedIndexChanged += new System.EventHandler(this.clients_SelectedIndexChanged);
+            // 
+            // btnPlayPause
+            // 
+            this.btnPlayPause.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlayPause.Location = new System.Drawing.Point(12, 5);
+            this.btnPlayPause.Name = "btnPlayPause";
+            this.btnPlayPause.Size = new System.Drawing.Size(200, 50);
+            this.btnPlayPause.TabIndex = 2;
+            this.btnPlayPause.Text = "▶";
+            this.btnPlayPause.UseVisualStyleBackColor = true;
+            this.btnPlayPause.Click += new System.EventHandler(this.btnPlayPause_Click);
+            // 
+            // textBoxDataPreview
+            // 
+            this.textBoxDataPreview.Location = new System.Drawing.Point(226, 506);
+            this.textBoxDataPreview.Multiline = true;
+            this.textBoxDataPreview.Name = "textBoxDataPreview";
+            this.textBoxDataPreview.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxDataPreview.Size = new System.Drawing.Size(732, 177);
+            this.textBoxDataPreview.TabIndex = 1;
+            // 
+            // FormLog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 695);
+            this.Controls.Add(this.textBoxDataPreview);
+            this.Controls.Add(this.btnPlayPause);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.messages);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormLog";
+            this.Text = "Log Client";
+            this.messages.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel messages;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox clients;
+        private System.Windows.Forms.Button btnPlayPause;
+        private System.Windows.Forms.TextBox textBoxDataPreview;
+    }
+}
+
---
+++ LogClient/FormLog.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogClient
+{
+    public partial class FormLog : Form
+    {
+        UdpClient client;
+        IPEndPoint listenEndPoint;
+        List<Data> dataList;
+        bool realtime = true;
+        public FormLog()
+        {
+            InitializeComponent();
+            dataList = new List<Data>();
+            clients.Items.Add("ALL");
+            clients.SetSelected(0, true);
+            listenEndPoint = new IPEndPoint(IPAddress.Any, int.Parse(ConfigurationManager.AppSettings["port"]));
+            client = new UdpClient(int.Parse(ConfigurationManager.AppSettings["port"]));
+            try
+            {
+                client.BeginReceive(new AsyncCallback(Receive), null);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+        }
+
+        void Receive(IAsyncResult res)
+        {
+            byte[] receivedBytes = client.EndReceive(res, ref listenEndPoint);
+            client.BeginReceive(new AsyncCallback(Receive), null);
+            string s = Encoding.UTF8.GetString(receivedBytes);
+            SimpleJSON.JSONNode node = SimpleJSON.JSON.Parse(s);
+            Data data = new Data(node["date"].Value, node["message"].Value, node["sender"].Value, node["receiver"].Value);
+            dataList.Add(data);
+            if(realtime)
+            if (MatchesSelectedCiteria(data))
+            {
+                dataGridView1.AllowUserToAddRows = true;
+                Invoke((Action<Data>)((_data) =>{bindingList.Insert(0, data);}),new object[] { data });
+            }
+            if (!clients.Items.Contains(data.sender))
+            {
+                clients.Invoke(new MethodInvoker(delegate { clients.Items.Add(data.sender);}));
+            }
+            foreach (var r in data.receiver)
+            {
+                if(!clients.Items.Contains(r))
+                    clients.Invoke(new MethodInvoker(delegate { clients.Items.Add(r); }));
+            }
+
+        }
+
+        bool MatchesSelectedCiteria(Data data)
+        {
+            return (bool)Invoke((Func<bool>)delegate
+            {
+                if (clients.SelectedIndex == 0)
+                {
+                    return true;
+                }
+                switch (clients.SelectedItems.Count)
+                {
+                    case 1:
+                        foreach (var re in data.receiver)
+                        {
+                            if (re == clients.SelectedItem.ToString())
+                                return true;
+                        }
+                        if (data.sender == clients.SelectedItem.ToString())
+                            return true;
+
+                        break;
+                    case 2:
+                        foreach (var re in data.receiver)
+                        {
+                            if ((re == clients.SelectedItems[0].ToString() &&
+                                 data.sender == clients.SelectedItems[1].ToString()) ||
+                                (re == clients.SelectedItems[1].ToString() &&
+                                 data.sender == clients.SelectedItems[0].ToString()))
+                                return true;
+                        }
+                        break;
+                    default:
+                        foreach (var re in data.receiver)
+                        {
+                            foreach (var c in clients.SelectedItems)
+                            {
+                                if (re == c)
+                                    return true;
+                            }
+                        }
+                        foreach (var c in clients.SelectedItems)
+                        {
+                            if (data.sender == c)
+                                return true;
+                        }
+                        break;
+                }
+                return false;
+            });
+        }
+
+        private void clients_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (clients.SelectedIndex == 0)
+            {
+                var r = (
+                    from d in dataList
+                    orderby d.dateTime descending
+                    select d).ToList();
+                bindingList = new BindingList<Data>(r);
+                dataGridView1.DataSource = bindingList;
+            }
+            else if (clients.SelectedItems.Count == 1)
+            {    
+                var r = (
+                    from d in dataList
+                    from re in d.receiver
+                    where (d.sender == clients.SelectedItem.ToString() || re == clients.SelectedItem.ToString())
+                    orderby d.dateTime descending
+                    select d).ToList();
+                bindingList = new BindingList<Data>(r);
+                dataGridView1.DataSource = bindingList;
+            }
+            else if (clients.SelectedItems.Count == 2)
+            {
+                var r = (
+                    from d in dataList
+                    from re in d.receiver
+                    where
+                        (re == clients.SelectedItems[0].ToString() &&
+                         d.sender == clients.SelectedItems[1].ToString()) ||
+                        (re == clients.SelectedItems[1].ToString() &&
+                         d.sender == clients.SelectedItems[0].ToString())
+                    orderby d.dateTime descending 
+                    select d).ToList();
+                bindingList = new BindingList<Data>(r);
+                dataGridView1.DataSource = bindingList;
+            }
+            else
+            {
+                string[] c = new string[clients.SelectedItems.Count];
+                for (int i = 0; i < clients.SelectedItems.Count; i++)
+                {
+                    c[i] = clients.SelectedItems[i].ToString();
+                }
+                var r = (
+                    from d in dataList
+                    from re in d.receiver
+                    where (c.Contains(re) || c.Contains(d.sender))
+                    orderby d.dateTime descending
+                    select d).ToList();
+                bindingList = new BindingList<Data>(r);
+                dataGridView1.DataSource = bindingList;
+            }
+        }
+
+        BindingList<Data> bindingList;
+
+        private void btnPlayPause_Click(object sender, EventArgs e)
+        {
+            realtime = !realtime;
+            if (realtime)
+            {
+                btnPlayPause.Text = "▶";
+                btnPlayPause.Font = new Font(FontFamily.GenericSansSerif, 30);
+                clients_SelectedIndexChanged(null, null);
+            }
+            else
+            {
+                btnPlayPause.Text = "■";
+                btnPlayPause.Font = new Font(FontFamily.GenericSansSerif, 20);
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 2)
+            {
+                textBoxDataPreview.Text = dataGridView1[e.ColumnIndex, e.RowIndex].Value.ToString();
+            }
+        }
+    }
+
+    public struct Data
+    {
+        public string sender;
+        public string[] receiver;
+        public string message;
+        public string dateTime;
+
+        public Data(string dt, string m, string s, string r)
+        {
+            dateTime = dt;
+            message = m;
+            sender = s;
+            receiver = r.Split(',').Select(p=>p.Trim()).Where(p => p != "").ToArray();
+            
+        }
+
+        public string Sender {
+            get { return sender; }
+        }
+        public string Receiver {
+            get { return string.Join(", ", receiver); }
+        }
+        public string Message
+        {
+            get { return message; }
+        }
+        public string DateTime
+        {
+            get { return dateTime; }
+        }
+    }
+}
